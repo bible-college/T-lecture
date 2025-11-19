0b8a7a6d36376c2423514ccad5e3ffa874b93420
naktan02 <jmgjmg102@gmail.com>
2025-11-20 01:12:20 +0900

    카카오 내비 test완료

--- server/src/config/index.js
+++ server/src/config/index.js
@@ -2,7 +2,6 @@ module.exports = {
   port: process.env.PORT || 3000,
   databaseUrl: process.env.DATABASE_URL || 'mysql://user:password@host:port/database',
   kakao: {
-    apiKey: process.env.KAKAO_API_KEY,
     restApiKey: process.env.KAKAO_REST_API_KEY,
   },
   nodeEnv: process.env.NODE_ENV || 'development',
